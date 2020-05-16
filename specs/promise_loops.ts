47131c0c67d7138a5e19ec0280a4c2f1812ea6a3
Chris Just <cpjust@hotmail.com>
2020-05-16 01:44:31 -0400

    Fixed promise rejections & added using() to tests.

--- specs/promise_loops.ts
+++ specs/promise_loops.ts
@@ -1,66 +1,85 @@
 import { logger } from "../config";
-import { browser, protractor } from 'protractor';
-import { sleep } from "../utils/timing";
-import { nativePromise } from "../utils/promises";
+import { printLater, nativePromise, protractorPromise } from "../utils/promises";
+
+const using = require('jasmine-data-provider');
 
 const TIMEOUT = 10000;
 
+const functions = [
+        {
+            name: 'printLater',
+            ptr: printLater
+        },
+        {
+            name: 'nativePromise',
+            ptr: nativePromise
+        },
+        {
+            name: 'protractorPromise',
+            ptr: protractorPromise
+        },
+];
+
 describe("Testing promises in loops...", () => {
-    const testPromiseChain = "No loops, just promise chaining.";
-    it(testPromiseChain, (done) => {
-        logger.info(testPromiseChain);
-        return nativePromise("First", 3000)
-            .then(() => nativePromise("Boom!", -1))
-            .then(() => nativePromise("Second", 2000))
-            .then(() => nativePromise("Third", 1000))
-            .then(() => done())
-            .catch(() => done.fail());
-    }, TIMEOUT);
+    using(functions, (data) => {
+        const func = data.ptr;
+        const funcName = data.name;
 
-    const testArrayReduce = "Promise in Array.reduce().";
-    it(testArrayReduce, (done) => {
-        logger.info(testArrayReduce);
-        let nums = [1, 2, 3];
+        const testPromiseChain = `[${funcName}] - No loops, just promise chaining.`;
+        it(testPromiseChain, (done) => {
+            logger.info(testPromiseChain);
+            return func("First", 2000)
+                .then(() => func("Boom!", -1))
+                .then(() => func("Second", 1000))
+                .then(() => done())
+                .catch(() => done.fail());
+        }, TIMEOUT);
 
-        return nums.reduce((promise, next) => {
+        const testArrayReduce = `[${funcName}] - Promise in Array.reduce().`;
+        it(testArrayReduce, (done) => {
+            logger.info(testArrayReduce);
+            let nums = [1, 2];
+
+            return nums.reduce((promise, next) => {
+                return promise
+                    .then(() => func("Loop #" + next, (nums.length + 1 - next) * 1000))
+                    .then(() => func("Boom!", -1));
+            }, Promise.resolve())
+                .then(() => done())
+                .catch(() => done.fail());
+        }, TIMEOUT);
+
+        const testForLoopInThen = `[${funcName}] - Promise in a for loop in a then().`;
+        it(testForLoopInThen, (done) => {
+            logger.info(testForLoopInThen);
+            let promise = Promise.resolve("");
+
+            for (let i = 2, j = 1; i > 0; --i, ++j) {
+                promise = promise
+                    .then(() => func("Loop #" + j, i * 1000))
+                    .then(() => func("Boom!", -1));
+            }
+
             return promise
-                .then(() => nativePromise("Loop #" + next, (nums.length + 1 - next) * 1000))
-                .then(() => nativePromise("Boom!", -1));
-        }, Promise.resolve())
-            .then(() => done())
-            .catch(() => done.fail());
-    }, TIMEOUT);
+                .then(() => done())
+                .catch(() => done.fail());
+        }, TIMEOUT);
 
-    const testForLoopInThen = "Promise in a for loop in a then().";
-    it(testForLoopInThen, (done) => {
-        logger.info(testForLoopInThen);
-        let promise = Promise.resolve("");
+        const testForLoop = `[${funcName}] - Promise in a for loop.`;
+        it(testForLoop, (done) => {
+            logger.info(testForLoop);
+            let promise = Promise.resolve("");
 
-        for (let i = 3, j = 1; i > 0; --i, ++j) {
-            promise = promise
-                .then(() => nativePromise("Loop #" + j, i * 1000))
-                .then(() => nativePromise("Boom!", -1));
-        }
+            for (let i = 2, j = 1; i > 0; --i, ++j) {
+                promise = promise
+                    .then(() => func("Loop #" + j, i * 1000))
+                    .then(() => func("Boom!", -1));
+            }
 
-        return promise
-            .then(() => done())
-            .catch(() => done.fail());
-    }, TIMEOUT);
+            return promise
+                .then(() => done())
+                .catch(() => done.fail());
+        }, TIMEOUT);
 
-    const testForLoop = "Promise in a for loop.";
-    it(testForLoop, (done) => {
-        logger.info(testForLoop);
-        let promise = Promise.resolve("");
-
-        for (let i = 3, j = 1; i > 0; --i, ++j) {
-            promise = promise
-                .then(() => nativePromise("Loop #" + j, i * 1000))
-                .then(() => nativePromise("Boom!", -1));
-        }
-
-        return promise
-            .then(() => done())
-            .catch(() => done.fail());
-    }, TIMEOUT);
-
+    });
 });
--- utils/promises.ts
+++ utils/promises.ts
@@ -1,34 +1,34 @@
 import { logger } from "../config";
 import { browser, protractor } from 'protractor';
+import { rejects } from "assert";
 
-function printLater(msg: string, time: number): Promise<string> {
+export function printLater(msg: string, time: number): Promise<string> {
     logger.debug("Sleeping for " + time + " ms...");
     return Promise.resolve("")
-        .then(() => browser.sleep(time)
-            .catch((err) => Promise.resolve("")))
-        // .then(() => {
-        //     setTimeout(() => {
-        //         logger.info(msg);
-        //     }, time)
-        // })
-        .then(() => logger.info(msg))
         .then(() => {
             if (time < 0) {
                 return Promise.reject("time cannot be < 0!");
             }
             return null;
         })
+        .then(() => browser.sleep(time)
+            .catch((err) => Promise.resolve("")))
+        .then(() => logger.info(msg))
         .then(() => msg);
 };
 
 export function nativePromise(msg: string, time: number): Promise<string> {
     return new Promise((resolve, reject) => {
-        return printLater(msg, time).then(() => resolve("done"));
+        return printLater(msg, time)
+            .then(() => resolve("done"))
+            .catch((err) => reject(err));
     });
 };
 
-function protractorPromise(msg: string, time: number) {
+export function protractorPromise(msg: string, time: number) {
     return new protractor.promise.Promise((resolve, reject) => {
-        printLater(msg, time).then(() => resolve("done"));
+        printLater(msg, time)
+            .then(() => resolve("done"))
+            .catch((err) => reject(err));
     });
 };
